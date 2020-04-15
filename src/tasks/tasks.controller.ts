1fca391077ae4abfcc0b7a88a71e1843cc8a75f0
Anastasiia Chorna <achorn@softserveinc.com>
2020-04-15 11:50:29 +0300

    Add get handler getTasksWithFilter by status and search string

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,20 @@
-import {Controller, Get, Post, Body, Param, Delete, Patch} from '@nestjs/common';
+import {Controller, Get, Post, Body, Param, Delete, Patch, Query} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
 import {Task, TaskStatus} from "./task.model";
 import {createTaskDto} from "./dto/create-task.dto";
+import {getTasksFilterDto} from "./dto/get-tasks-filter.dto";
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks()
+    getTasks(@Query() filterTasks: getTasksFilterDto): Task[] {
+        if (Object.keys(filterTasks).length) {
+            return this.tasksService.getTasksFilter(filterTasks);
+        } else {
+            return this.tasksService.getAllTasks();
+        }
     }
 
     @Get('/:id')
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import {Task, TaskStatus} from './task.model';
 import * as uuid from 'uuid/v1'
 import {createTaskDto} from "./dto/create-task.dto";
+import {getTasksFilterDto} from "./dto/get-tasks-filter.dto";
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,19 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    getTasksFilter(filterDto: getTasksFilterDto): Task[] {
+        let tasks = this.getAllTasks();
+        if (filterDto.status) {
+            tasks = tasks.filter(task => task.status === filterDto.status);
+        }
+        if (filterDto.search) {
+            tasks = tasks.filter(task => task.title.includes(filterDto.search)
+                                                || task.description.includes(filterDto.search));
+        }
+
+        return tasks
     }
 
     getTaskById(id: string): Task {
